c8f883e5d93b5f0e2253c95a387fe265c2a0f8ae
HPSIT <lymivanluk1110@gmail.com>
2024-05-26 10:01:17 +0800

    main.ts

--- main.ts
+++ main.ts
@@ -2148,7 +2148,7 @@ function Now () {
 }
 function Hi () {
     game.showLongText("Use keyboard to play lol\nKeyboard Controls\nSpacebar: Button A\nUp button: Jump\\nRight Button: Move to Right\\nLeft Button: Move to Left\\nButton B: Change Character", DialogLayout.Full)
-    game.showLongText("If you do not know how to play, please read the instructions")
+    game.showLongText("If you do not know how to play, please read the instructions", DialogLayout.Full)
     story.showPlayerChoices("How To Play", "Start Now")
     if (story.checkLastAnswer("Start Now")) {
         Now()
